21a95df01954380c23e588be47740f257b2219b1
irineumouracaldeira <121524120+irineumouracaldeira@users.noreply.github.com>
2024-08-14 15:37:07 -0300

    Add files via upload

---
+++ TestaIdade.java
@@ -0,0 +1,13 @@
+class TestaIdade {
+	public static void manin(String[] args) {
+	// imprime a idade
+	System.out.println(idade);
+
+	// gera uma idade no ano seguinte
+	int idadeNoAnoQueVem;
+	idadeNoAnoQueVem = idade + 1;
+
+	// imprime a idade
+	System.out.println(idadeNoAnoQueVem);
+	}
+}
